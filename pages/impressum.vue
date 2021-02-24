fb56bd504f28181e388e93fb407c13200c4c030c
Moritz <moritz@dialogik.tv>
2021-02-24 13:52:08 +0100

    Improve imprint layout

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -1,16 +1,18 @@
 <template>
-     <div class="container bg-light text-dark">
-          <h1>Impressum</h1>
-          <p>
-               dialogikTV<br>
-               c/o AutorenServices.de<br>
-               Birkenallee 24<br>
-               36037 Fulda<br>
-               <br>
-               <a href="https://dialogik.tv">dialogik.tv</a><br>
-               <a href="mailto:kontakt@dialogik.tv">kontakt@dialogik.tv</a>
-          </p>
-          <hr>
-          <p><n-link :to="'/datenschutz'">Datenschutzerklärung</n-link></p>
+     <div class="container-fluid bg-secondary text-light">
+          <div class="container">
+               <h1>Impressum</h1>
+               <p>
+                    dialogikTV<br>
+                    c/o AutorenServices.de<br>
+                    Birkenallee 24<br>
+                    36037 Fulda<br>
+                    <br>
+                    <a href="https://dialogik.tv">dialogik.tv</a><br>
+                    <a href="mailto:kontakt@dialogik.tv">kontakt@dialogik.tv</a>
+               </p>
+               <hr>
+               <p><n-link :to="'/datenschutz'">Datenschutzerklärung</n-link></p>
+          </div>
      </div>
 </template>
